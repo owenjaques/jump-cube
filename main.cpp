f8dc95fb471bf2b830ba5a6742df50807fe97ea4
owenjaques <owenjaques2000@gmail.com>
2019-12-22 13:21:20 -0800

    got basic sprite loading

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ const int SCREEN_WIDTH = 720;
 int main(int argv, char* args[]){
 	SDL_Window* window = NULL;
 	SDL_Surface* screen_surface = NULL;
-	if(SDL_Init(SDL_INIT_VIDEO) == -1){
+	SDL_Surface* the_image = NULL;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0){
 		printf("SDL has returned an error initializing\n");
 		return 1;
 	}
@@ -18,7 +19,18 @@ int main(int argv, char* args[]){
 		printf("SDL was unable to create the window\n");
 		return 1;
 	}
-	while(1);
+	screen_surface = SDL_GetWindowSurface(window);
+	the_image = SDL_LoadBMP("images/player.bmp");
+	SDL_BlitSurface(the_image, NULL, screen_surface, NULL);
+	SDL_UpdateWindowSurface(window);
+
+
+
+	SDL_Delay(2000);
+	
+	//clean up
+	SDL_DestroyWindow(window);
+    SDL_Quit();
 	return 0;
 }
 
