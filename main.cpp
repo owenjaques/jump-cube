f33ab19a93c0cf28a381b4122904369317ada123
owenjaques <owenjaques2000@gmail.com>
2019-12-22 12:54:07 -0800

    got a window launching

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
-#include "SDL.h"
+#include <stdio.h>
+#include <SDL.h>
 
 using namespace std;
 
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 720;
+
 int main(int argv, char* args[]){
-	cout << "Hello world\n";
+	SDL_Window* window = NULL;
+	SDL_Surface* screen_surface = NULL;
+	if(SDL_Init(SDL_INIT_VIDEO) == -1){
+		printf("SDL has returned an error initializing\n");
+		return 1;
+	}
+	window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if(!window){
+		printf("SDL was unable to create the window\n");
+		return 1;
+	}
+	while(1);
 	return 0;
 }
 
